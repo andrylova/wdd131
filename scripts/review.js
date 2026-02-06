596bb3554a3d1c31d61b35ad0ba7618a99f612ee
Ton Nom Complet <tonemail@gmail.com>
2026-02-06 15:39:37 +0300

    W05 assignment: Form

---
+++ scripts/review.js
@@ -0,0 +1,13 @@
+let count = localStorage.getItem("reviewCount");
+
+
+if (!count) {
+count = 0;
+}
+
+
+count++;
+localStorage.setItem("reviewCount", count);
+
+
+document.getElementById("reviewCount").textContent = count;
